5c50ed4c48e960cd1a39f89b2f1b191df44ab410
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2023-03-16 20:46:11 +0100

    Tabs system

---
+++ tabs/script.js
@@ -0,0 +1,13 @@
+function openCity(event, tab) {
+    
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tab).style.display = "block";
+    event.currentTarget.className += " active";
+}
